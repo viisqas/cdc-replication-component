773cd2eb6575ace0a565c8de087a42df78c130a6
Viktoriya Kalinina <vsq.kalinina@gmail.com>
2019-04-21 16:23:41 +0300

    draft version connect and get

---
+++ CDC_demo/DBSQLServerUtils.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace CDC_demo
+{
+    class DBSQLServerUtils
+    {
+        public static SqlConnection GetDBConnection()
+        {
+            string connString = "Data Source=VIKTORIYAKA5408;Initial Catalog=CDC_demo;Integrated Security=True";
+            SqlConnection conn = new SqlConnection(connString);
+            
+            try
+            {
+                Console.WriteLine("Openning Connection ...");
+
+                conn.Open();
+
+                Console.WriteLine("Connection successful!");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error: " + e.Message);
+            }
+
+            return conn;
+        }
+    }
+}
--- CDC_demo/Program.cs
+++ CDC_demo/Program.cs
@@ -1,4 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data.Common;
 
 namespace CDC_demo
 {
@@ -6,7 +12,34 @@ namespace CDC_demo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //DB connect
+            SqlConnection conn = DBSQLServerUtils.GetDBConnection();
+
+            //Get data
+            string sql = "Select * from PersonalInfo";
+
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = conn;
+            cmd.CommandText = sql;
+
+
+            using (DbDataReader reader = cmd.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    int clientIdIndex = reader.GetOrdinal("ClientId");
+                    long clientId = Convert.ToInt64(reader.GetValue(0));
+
+                    string clientNo = reader.GetString(1);
+                    int clientNameIndex = reader.GetOrdinal("FirstName");
+                    string clientName = reader.GetString(clientNameIndex);
+
+                    Console.WriteLine(clientName);
+                    Console.WriteLine(sql);
+                }
+            }
+
+            Console.Read();
         }
     }
 }
